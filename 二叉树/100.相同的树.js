720603367f20be32638debae34a54108cfb70212
tyrone <1161763410@qq.com>
2020-07-12 10:36:14 +0800

    100

--- 二叉树/100.相同的树.js
+++ 二叉树/100.相同的树.js
@@ -2,6 +2,10 @@
  * @lc app=leetcode.cn id=100 lang=javascript
  *
  * [100] 相同的树
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 思路：比较当前的节点是否相同，在比较左右节点的值是否相同，并依次递归
  */
 
 // @lc code=start
@@ -17,8 +21,13 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-
+var isSameTree = function (p, q) {
+	let isSame = (l, r) => {
+		if (!l && !r) return true;
+		if ((!l && r) || (l && !r)) return false;
+		if (l.val != r.val) return false;
+		return isSame(l.left, r.left) && isSame(l.right, r.right);
+	};
+	return isSame(p, q);
 };
 // @lc code=end
-
